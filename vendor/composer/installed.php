20f2e34b9989d0c305ba79c36dd2f7f4c737d10b
AL AMIN <hmalaminmb4@gmail.com>
2024-01-05 16:27:11 +0600

    Added main file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dev-alamin/book-review',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ff94418b874923d982e7ec5014ee7aaa728d236d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dev-alamin/book-review' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ff94418b874923d982e7ec5014ee7aaa728d236d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ wp-book-review.php
@@ -0,0 +1,126 @@
+<?php 
+/**
+ * Plugin Name: Book Review
+ * Plugin URI:  
+ * Description: This plugin will help you to take user review like forum. It will increase engangement and thus boost sale.
+ * Version:     1.0
+ * Author:      Al Amin, Fahad Abdullah
+ * Author URI:  https://almn.me/wp-book-review
+ * Text Domain: wbr
+ * Domain Path: /languages
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * Requires at least: 5.4
+ * Requires PHP: 7.0
+ * Requires Plugins:
+ *
+ * @package     WPBookReview
+ * @author      Al Amin, Fahad Abdullah
+ * @copyright   2024 ShalikDev
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ *
+ * Prefix:      Book_Review
+ */
+
+
+ if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * The main plugin class
+ */
+final class Book_Review {
+
+    /**
+     * Plugin version
+     *
+     * @var string
+     */
+    const version = '1.0';
+
+    /**
+     * Class construcotr
+     */
+    private function __construct() {
+        $this->define_constants();
+
+        register_activation_hook( __FILE__, [ $this, 'activate' ] );
+
+        add_action( 'plugins_loaded', [ $this, 'init_plugin' ] );
+    }
+
+    /**
+     * Initializes a singleton instance
+     *
+     * @return \Book_Review
+     */
+    public static function init() {
+        static $instance = false;
+
+        if ( ! $instance ) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+
+    /**
+     * Define the required plugin constants
+     *
+     * @return void
+     */
+    public function define_constants() {
+        define( 'BOOK_REVIEW_VERSION', self::version );
+        define( 'BOOK_REVIEW_FILE', __FILE__ );
+        define( 'BOOK_REVIEW_PATH', __DIR__ );
+        define( 'BOOK_REVIEW_URL', plugins_url( '', BOOK_REVIEW_FILE ) );
+        define( 'BOOK_REVIEW_ASSETS', WD_ACADEMY_URL . '/assets' );
+    }
+
+    /**
+     * Initialize the plugin
+     *
+     * @return void
+     */
+    public function init_plugin() {
+
+        // new Book\Review\Assets();
+
+        // if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+        //     new Book\Review\Ajax();
+        // }
+
+        if ( is_admin() ) {
+            new Book\Review\Admin();
+        } else {
+            new Book\Review\Frontend();
+        }
+    }
+
+    /**
+     * Do stuff upon plugin activation
+     *
+     * @return void
+     */
+    public function activate() {
+
+    }
+}
+
+/**
+ * Initializes the main plugin
+ *
+ * @return \Book_Review
+ */
+function Book_Review() {
+    return Book_Review::init();
+}
+
+// kick-off the plugin
+Book_Review();
